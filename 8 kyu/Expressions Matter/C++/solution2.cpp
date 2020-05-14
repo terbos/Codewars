288d6f59b4700f8ef7ea505e86de2983ea185c57
Yiannis Papastergiou <terbos@users.noreply.github.com>
2020-05-14 11:06:32 +0100

    solution2.cpp

---
+++ 8 kyu/Expressions Matter/C++/solution2.cpp
@@ -0,0 +1,6 @@
+#include <algorithm>
+
+unsigned short expressionsMatter(unsigned short a, unsigned short b, unsigned short c) 
+{
+  return std::max({a + b + c, a * (b + c), (a + b) * c, a * b * c});
+}
